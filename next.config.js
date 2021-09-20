3e1baca06dbb4aaeafd9403c420d0b7d48a2a8c2
Ludoviko <me@ludoviko.xyz>
2021-09-20 22:45:24 +0100

    Switched image loaders

--- next.config.js
+++ next.config.js
@@ -24,6 +24,8 @@ module.exports = withTM({
     webpack5: true,
     images: {
         domains: ['liberapay.com'],
+        loader: 'cloudinary',
+        path: 'https://res.cloudinary.com/ludoviko/image/upload/'
     },
     async headers() {
         return [
