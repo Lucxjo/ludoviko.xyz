3fa2fd9090cc74e648b2ab39f1c440761b824b52
Ludoviko <me@ludoviko.ch>
2022-04-30 16:02:05 +0100

    Updated pgp key for me@ Updated Discord page again

--- docker.sh
+++ docker.sh
@@ -1,4 +1,3 @@
-docker build -t ghcr.io/lucxjo/ludo-web:$1 .
+docker build -t ghcr.io/lucxjo/ludo-web:$1 -t ghcr.io/lucxjo/ludo-web:latest .
 docker push ghcr.io/lucxjo/ludo-web:$1
-docker build -t ghcr.io/lucxjo/ludo-web:latest .
 docker push ghcr.io/lucxjo/ludo-web:latest
