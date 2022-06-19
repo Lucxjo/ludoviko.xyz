fc232999a0cf3e1f0dcac3ce37ff1433f53c330f
Louis Hollingworth <louis@hollingworth.ch>
2022-06-19 21:34:31 +0100

    Added project cards

--- components/Card.vue
+++ components/Card.vue
@@ -1,3 +1,56 @@
 <template>
-    <div></div>
+    <div class="card">
+        <h2>{{ title }}</h2>
+        <p>{{ desc }}</p>
+        <div v-for="link in links" :key="link">
+            <a v-if="link.isExternal" target="_blank" rel="nofollow noreferrer" :href="link.to">
+		        <p>{{ link.title }} →</p>
+	        </a>
+	        <NuxtLink v-else v-bind="$props" :to="link.to">
+		        <p>{{ link.title }} →</p>
+	        </NuxtLink>
+        </div>
+    </div>
 </template>
+
+<script>
+const Card = resolveComponent('Card');
+
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        desc: {
+            type: String,
+            required: true
+        },
+        links: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+
+    border-style: solid;
+    border-width: 0.2rem;
+    border-radius: 0.5rem;
+    border-color: #39c4ff;
+    padding-left: 1rem;
+}
+
+a {
+    color: azure;
+    transition: color 0.15s ease, color 0.15s ease;
+}
+
+a:hover {
+    color: #39c4ff;
+    transition: color 0.15s ease, color 0.15s ease;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -15,8 +15,30 @@
                 <li>Languages: en-GB, es-ES, eo-EO</li>
             </ul>
         </Sect>
+        <Sect title="Projects" sub="" id="projects">
+            <Card
+                title="VannerBasta"
+                desc="A website to promote the Netflix TV programme 'Young Royals'. Not seen it yet? You totally should, there's a link on this website that will take you straight to Netflix!"
+                :links="projLinks">
+                </Card>
+        </Sect>
     </div>
 </template>
+
+<script setup>
+const projLinks = [
+    {
+        title: 'Source',
+        to: 'https://github.com/Lucxjo/friends-best',
+        isExternal: true
+    },
+    {
+        title: 'View',
+        to: 'https://vannerba.st/',
+        isExternal: true
+    },
+]
+</script>
 
 <style lang="scss" scoped>
 .hero {
