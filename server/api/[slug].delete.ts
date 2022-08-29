81b998f6b035e0a7cae01fdff0e19808b423f45d
Louis Hollingworth <louis@hollingworth.ch>
2022-08-29 19:45:29 +0100

    (maint,#8) File formatting
    Signed-off-by: Louis Hollingworth <louis@hollingworth.ch>

--- server/api/[slug].delete.ts
+++ server/api/[slug].delete.ts
@@ -1,15 +1,16 @@
-
 export default defineEventHandler(async (event) => {
 	event.res.setHeader("Content-Type", "application/json");
 
 	await prisma.shortLink.delete({
 		where: {
 			slug: event.context.params.slug,
-		}
-	})
+		},
+	});
 
 	return JSON.stringify({
 		message: "Success",
-		data: await prisma.shortLink.findUnique({ where: { slug: event.context.params.slug } }),
+		data: await prisma.shortLink.findUnique({
+			where: { slug: event.context.params.slug },
+		}),
 	});
-})
+});
